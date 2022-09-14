eaf0386395fe1310634d8eb7ee5fdb0ae426a0db
pring.les <pring.les@kakaocorp.com>
2022-09-15 06:46:27 +0900

    feat: Step1 구현
    - Request 를 분석하여, Path 를 찾아 해당 파일 반환하도록 작성
    - 요청 데이터가 존재하지 않을 시, Hello World 출력으로 대응

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -18,14 +17,28 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
+    private String getPath(String s) {
+        return s.split(" ")[1];
+    }
+
+    private byte[] getBody(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        String path = getPath(br.readLine());
+
+        try {
+            return Files.readAllBytes(new File("./webapp" + path).toPath());
+        } catch (IOException e) {
+            return "Hello World".getBytes();
+        }
+    }
+
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = getBody(in);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
