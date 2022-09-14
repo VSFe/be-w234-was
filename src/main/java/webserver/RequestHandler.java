5e2a71c39afb9641efa6c78d814ffd76c9c43a09
pring.les <pring.les@kakaocorp.com>
2022-09-15 08:20:35 +0900

    refactor: RequestHandler 분리
    - RequestHandler 내 getPath, getBody 분리
    - RequestUtil class 로 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,10 +3,11 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -17,34 +18,13 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
-    private String getPath(String s) {
-        return s.split(" ")[1];
-    }
-
-    private byte[] getBody(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-        String path = getPath(br.readLine());
-        String log = path;
-
-        try {
-            while (!"".equals(log)) {
-                logger.debug(log);
-                log = br.readLine();
-            }
-
-            return Files.readAllBytes(new File("./webapp" + path).toPath());
-        } catch (IOException e) {
-            return "Hello World".getBytes();
-        }
-    }
-
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = getBody(in);
+            byte[] body = RequestUtil.getBody(in);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
