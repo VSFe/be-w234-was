02073a0e5ce6a97ae1f34e10c18fb0085cbd4785
VSFe <klm03025@gmail.com>
2022-09-19 00:57:30 +0900

    refactor: HttpRequestUtils.Pair.equals() 수정
    - depth가 깊어지는 문제 해결

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -85,25 +85,11 @@ public class HttpRequestUtils {
         }
 
         @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Pair other = (Pair) obj;
-            if (key == null) {
-                if (other.key != null)
-                    return false;
-            } else if (!key.equals(other.key))
-                return false;
-            if (value == null) {
-                if (other.value != null)
-                    return false;
-            } else if (!value.equals(other.value))
-                return false;
-            return true;
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return Objects.equals(key, pair.key) && Objects.equals(value, pair.value);
         }
 
         @Override
