abc6e82c15de82c978dff6da7cedf6b1cc177d7a
VSFe <klm03025@gmail.com>
2022-09-19 21:47:53 +0900

    feat: 요청에 따른 Controller 를 실행하는 Resolver 추가
    - resolve() 메서드 추가

---
+++ src/main/java/webserver/controller/ControllerResolver.java
@@ -0,0 +1,33 @@
+package webserver.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.enums.HttpMethod;
+import webserver.enums.HttpStatus;
+import webserver.request.HttpRequest;
+import webserver.request.HttpRequestUtil;
+import webserver.response.HttpResponse;
+
+import java.io.IOException;
+
+public class ControllerResolver {
+    private static Logger logger = LoggerFactory.getLogger(ControllerResolver.class);
+    private UserController userController;
+
+    public ControllerResolver() {
+        userController = new UserController();
+    }
+
+    public HttpResponse resolve(HttpRequest httpRequest) throws IOException {
+        String url = httpRequest.getDirectory().split("\\?")[0];
+        HttpMethod method = httpRequest.getHttpMethod();
+        logger.debug(url);
+
+        // 이후 다양한 Controller에 대한 Resolve
+        return new HttpResponse.Builder()
+                .setContentType(httpRequest.getMIMEType() + ";charset=utf-8")
+                .setHttpStatus(HttpStatus.OK)
+                .setBody(HttpRequestUtil.getBody(httpRequest.getDirectory()))
+                .build();
+    }
+}
