cbf63995273b4468c4e033505400a21108acff2a
VSFe <klm03025@gmail.com>
2022-09-19 21:46:15 +0900

    refactor: HttpRequestUtil.getBody() 메서드 수정
    - HttpRequest 를 직접 받지 않고, String을 받도록 수정

--- src/main/java/webserver/request/HttpRequestUtil.java
+++ src/main/java/webserver/request/HttpRequestUtil.java
@@ -5,9 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class HttpRequestUtil {
-    public static byte[] getBody(HttpRequest httpRequest) throws IOException {
+    public static byte[] getBody(String path) throws IOException {
         try {
-            String path = httpRequest.getDirectory();
             return Files.readAllBytes(new File("./webapp" + path).toPath());
         } catch (IOException e) {
             return "Hello World".getBytes();
