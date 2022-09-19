3bd40b6327b0424e459a8f887b769bb0475e27df
VSFe <klm03025@gmail.com>
2022-09-19 22:06:03 +0900

    test: 프로젝트 구조 변경에 따른 테스트 코드 변경
    - HttpRequestUtilTest 변경

--- src/test/java/webserver/request/HttpRequestUtilTest.java
+++ src/test/java/webserver/request/HttpRequestUtilTest.java
@@ -1,15 +1,14 @@
-package webserver;
+package webserver.request;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import webserver.request.HttpRequestUtil;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RequestHandlerUtilTest {
+public class HttpRequestUtilTest {
 
     @Test
     @DisplayName("Request로 부터 도메인을 확인할 수 있다.")
@@ -24,8 +23,9 @@ public class RequestHandlerUtilTest {
     @DisplayName("getBody() 사용 시, 존재하는 파일의 경우 읽고 반환할 수 있다.")
     void readValidFile() throws IOException {
         InputStream is = new ByteArrayInputStream("GET /index.html HTTP/1.1\nHost: localhost:8080\nConnection: keep-alive\nAccept: */*".getBytes());
+        String path = HttpRequestUtil.getPath(new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8)).readLine());
 
-        String s = new String(HttpRequestUtil.getBody(is), StandardCharsets.UTF_8);
+        String s = new String(HttpRequestUtil.getBody(path), StandardCharsets.UTF_8);
 
         // html 문서가 정상적으로 로드 되었는가?
         assertThat(s).contains("<!DOCTYPE html>");
@@ -35,8 +35,9 @@ public class RequestHandlerUtilTest {
     @DisplayName("getBody() 사용 시, 존재하지 않은 파일의 경우 Hello World를 표시한다.")
     void readInvalidFile() throws IOException {
         InputStream is = new ByteArrayInputStream("GET /notIndex.html HTTP/1.1\nHost: localhost:8080\nConnection: keep-alive\nAccept: */*".getBytes());
+        String path = HttpRequestUtil.getPath(new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8)).readLine());
 
-        String s = new String(HttpRequestUtil.getBody(is), StandardCharsets.UTF_8);
+        String s = new String(HttpRequestUtil.getBody(path), StandardCharsets.UTF_8);
 
         assertThat(s).isEqualTo("Hello World");
     }
