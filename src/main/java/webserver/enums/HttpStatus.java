903fb0e793e5781414f20c7269f3d2ee86435e12
VSFe <klm03025@gmail.com>
2022-09-20 23:47:42 +0900

    feat: HttpStatus 302 추가

--- src/main/java/webserver/enums/HttpStatus.java
+++ src/main/java/webserver/enums/HttpStatus.java
@@ -3,6 +3,7 @@ package webserver.enums;
 public enum HttpStatus {
     OK(200, "OK"),
     CREATED(201, "Created"),
+    FOUND(302, "Found"),
     NOT_FOUND(404, "Not Found");
 
     private final int code;
